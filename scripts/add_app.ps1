139f08107bee340bbabad3beaff77cee13a6881d
joe.guinto <joe.guinto@adt.com.au>
2022-03-02 18:08:39 +1100

    Initial implementation of adding application

--- scripts/add_app.ps1
+++ scripts/add_app.ps1
@@ -14,6 +14,190 @@ $azureStorageUploadChunkSizeInMb = 6l;
 $sleep = 30
 ##
 
+function FinalizeAzureStorageUpload($sasUri, $ids){
+
+	$uri = "$sasUri&comp=blocklist";
+	$request = "PUT $uri";
+
+	$xml = '<?xml version="1.0" encoding="utf-8"?><BlockList>';
+	foreach ($id in $ids)
+	{
+		$xml += "<Latest>$id</Latest>";
+	}
+	$xml += '</BlockList>';
+
+	if ($logRequestUris) { Write-Host $request; }
+	if ($logContent) { Write-Host -ForegroundColor Gray $xml; }
+
+	try
+	{
+		Invoke-RestMethod $uri -Method Put -Body $xml;
+	}
+	catch
+	{
+		Write-Host -ForegroundColor Red $request;
+		Write-Host -ForegroundColor Red $_.Exception.Message;
+		throw;
+	}
+}
+
+function RenewAzureStorageUpload($fileUri){
+
+	$renewalUri = "$fileUri/renewUpload";
+	$actionBody = "";
+	$rewnewUriResult = MakePostRequest $renewalUri $actionBody;
+	
+	$file = WaitForFileProcessing $fileUri "AzureStorageUriRenewal" $azureStorageRenewSasUriBackOffTimeInSeconds;
+
+}
+
+function UploadAzureStorageChunk($sasUri, $id, $body){
+
+	$uri = "$sasUri&comp=block&blockid=$id";
+	$request = "PUT $uri";
+
+	$iso = [System.Text.Encoding]::GetEncoding("iso-8859-1");
+	$encodedBody = $iso.GetString($body);
+	$headers = @{
+		"x-ms-blob-type" = "BlockBlob"
+	};
+
+	if ($logRequestUris) { Write-Host $request; }
+	if ($logHeaders) { WriteHeaders $headers; }
+
+	try
+	{
+		$response = Invoke-WebRequest $uri -Method Put -Headers $headers -Body $encodedBody;
+	}
+	catch
+	{
+		Write-Host -ForegroundColor Red $request;
+		Write-Host -ForegroundColor Red $_.Exception.Message;
+		throw;
+	}
+
+}
+
+function UploadFileToAzureStorage($sasUri, $filepath, $fileUri){
+
+	try {
+
+        $chunkSizeInBytes = 1024l * 1024l * $azureStorageUploadChunkSizeInMb;
+		
+		# Start the timer for SAS URI renewal.
+		$sasRenewalTimer = [System.Diagnostics.Stopwatch]::StartNew()
+		
+		# Find the file size and open the file.
+		$fileSize = (Get-Item $filepath).length;
+		$chunks = [Math]::Ceiling($fileSize / $chunkSizeInBytes);
+		$reader = New-Object System.IO.BinaryReader([System.IO.File]::Open($filepath, [System.IO.FileMode]::Open));
+		$position = $reader.BaseStream.Seek(0, [System.IO.SeekOrigin]::Begin);
+		
+		# Upload each chunk. Check whether a SAS URI renewal is required after each chunk is uploaded and renew if needed.
+		$ids = @();
+
+		for ($chunk = 0; $chunk -lt $chunks; $chunk++){
+
+			$id = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($chunk.ToString("0000")));
+			$ids += $id;
+
+			$start = $chunk * $chunkSizeInBytes;
+			$length = [Math]::Min($chunkSizeInBytes, $fileSize - $start);
+			$bytes = $reader.ReadBytes($length);
+			
+			$currentChunk = $chunk + 1;			
+
+            Write-Progress -Activity "Uploading File to Azure Storage" -status "Uploading chunk $currentChunk of $chunks" `
+            -percentComplete ($currentChunk / $chunks*100)
+
+            $uploadResponse = UploadAzureStorageChunk $sasUri $id $bytes;
+			
+			# Renew the SAS URI if 7 minutes have elapsed since the upload started or was renewed last.
+			if ($currentChunk -lt $chunks -and $sasRenewalTimer.ElapsedMilliseconds -ge 450000){
+
+				$renewalResponse = RenewAzureStorageUpload $fileUri;
+				$sasRenewalTimer.Restart();
+			
+            }
+
+		}
+
+        Write-Progress -Completed -Activity "Uploading File to Azure Storage"
+
+		$reader.Close();
+
+	}
+
+	finally {
+
+		if ($reader -ne $null) { $reader.Dispose(); }
+	
+    }
+	
+	# Finalize the upload.
+	$uploadResponse = FinalizeAzureStorageUpload $sasUri $ids;
+
+}
+
+function MakeGetRequest($collectionPath){
+
+	$uri = "$baseUrl$collectionPath";
+	$request = "GET $uri";
+	
+	if ($logRequestUris) { Write-Host $request; }
+	if ($logHeaders) { WriteHeaders $authToken; }
+
+	try
+	{
+		Test-AuthToken
+		$response = Invoke-RestMethod $uri -Method Get -Headers $authToken;
+		$response;
+	}
+	catch
+	{
+		Write-Host -ForegroundColor Red $request;
+		Write-Host -ForegroundColor Red $_.Exception.Message;
+		throw;
+	}
+}
+
+function WaitForFileProcessing($fileUri, $stage){
+
+	$attempts= 600;
+	$waitTimeInSeconds = 10;
+
+	$successState = "$($stage)Success";
+	$pendingState = "$($stage)Pending";
+	$failedState = "$($stage)Failed";
+	$timedOutState = "$($stage)TimedOut";
+
+	$file = $null;
+	while ($attempts -gt 0)
+	{
+		$file = MakeGetRequest $fileUri;
+
+		if ($file.uploadState -eq $successState)
+		{
+			break;
+		}
+		elseif ($file.uploadState -ne $pendingState)
+		{
+			Write-Host -ForegroundColor Red $_.Exception.Message;
+            throw "File upload state is not success: $($file.uploadState)";
+		}
+
+		Start-Sleep $waitTimeInSeconds;
+		$attempts--;
+	}
+
+	if ($file -eq $null -or $file.uploadState -ne $successState)
+	{
+		throw "File request did not complete in the allotted time.";
+	}
+
+	$file;
+}
+
 function CloneObject($object){
 
 	$stream = New-Object IO.MemoryStream;
@@ -298,10 +482,9 @@ function Get-TargetId {
         $name
     )
     
+    # TODO: maybe load from config?!?
     $map = @{
-        "Hardware" = "0"
-        "Software" = "1"
-        "Engineers" = "2"
+        "TestGroup" = "d251b7b9-707e-4ab6-af25-08ef9e447434"
     }
     
     $map.Get_Item($name)
@@ -432,6 +615,76 @@ function Add-Application {
     Write-Host
     Write-Host "Creating application in Intune..." -ForegroundColor Yellow
     $mobileApp = MakeRequest "POST" "mobileApps" ($appInfo.app | ConvertTo-Json)
+    
+    # Get the content version for the new app (this will always be 1 until the new app is committed).
+    Write-Host
+    Write-Host "Creating Content Version in the service for the application..." -ForegroundColor Yellow
+    $appId = $mobileApp.id
+    $contentVersionUri = "mobileApps/$appId/$LOBType/contentVersions"
+    $contentVersion = MakeRequest "POST" $contentVersionUri "{}"
+
+    # Create a new file for the app.
+    Write-Host
+    Write-Host "Creating a new file entry in Azure for the upload..." -ForegroundColor Yellow
+    $contentVersionId = $contentVersion.id
+    $filesUri = "mobileApps/$appId/$LOBType/contentVersions/$contentVersionId/files"
+    $file = MakeRequest "POST" $filesUri ($appInfo.file | ConvertTo-Json)
+    
+    # Wait for the service to process the new file request.
+    Write-Host
+    Write-Host "Waiting for the file entry URI to be created..." -ForegroundColor Yellow
+    $fileId = $file.id
+    $fileUri = "mobileApps/$appId/$LOBType/contentVersions/$contentVersionId/files/$fileId"
+    $file = WaitForFileProcessing $fileUri "AzureStorageUriRequest"
+    
+    # Upload the content to Azure Storage.
+    Write-Host
+    Write-Host "Uploading file to Azure Storage..." -f Yellow
+
+    $sasUri = $file.azureStorageUri
+    UploadFileToAzureStorage $file.azureStorageUri "$intuneWinFile" $fileUri
+
+    # Need to Add removal of IntuneWin file
+    $IntuneWinFolder = [System.IO.Path]::GetDirectoryName("$intuneWinFile")
+    Remove-Item "$intuneWinFile" -Force
+    
+    # Commit the file.
+    Write-Host
+    Write-Host "Committing the file into Azure Storage..." -ForegroundColor Yellow
+    $commitFileUri = "mobileApps/$appId/$LOBType/contentVersions/$contentVersionId/files/$fileId/commit";
+    MakeRequest "POST" $commitFileUri ($appInfo.commit | ConvertTo-Json);
+    
+    # Wait for the service to process the commit file request.
+    Write-Host
+    Write-Host "Waiting for the service to process the commit file request..." -ForegroundColor Yellow
+    $file = WaitForFileProcessing $fileUri "CommitFile";
+
+    # Commit the app.
+    Write-Host
+    Write-Host "Committing the file into Azure Storage..." -ForegroundColor Yellow
+    $commitAppUri = "mobileApps/$appId";
+    $commitAppBody = @{
+        "@odata.type" = "#$LOBType"
+        "committedContentVersion" = $contentVersionId
+    }
+    MakeRequest "PATCH" $commitAppUri ($commitAppBody | ConvertTo-Json);
+    
+    Write-Host "Sleeping for $sleep seconds to allow patch completion..." -f Magenta
+    Start-Sleep $sleep
+    Write-Host
+    
+    # Add assignments to app
+    Write-Host
+    Write-Host "Adding assignments..." -ForegroundColor Yellow
+    $listAssignmentsUri = "mobileApps/$appId/assignments"
+    foreach ($target in $appInfo.assignments)
+    {
+        MakeRequest "POST" $listAssignmentsUri $($target | ConvertTo-Json)
+    }
+    
+    # Done!
+    Write-Host
+    Write-Host "Done..." -ForegroundColor Yellow
 }
 
 # Testing
