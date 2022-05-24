bb51d41309dd6d2bb46d568d18a299447277c696
joe.guinto <joe.guinto@adt.com.au>
2022-05-24 18:06:11 +1000

    Adding seven-zed

--- scripts/apply_change.ps1
+++ scripts/apply_change.ps1
@@ -1,6 +1,6 @@
 Import-Module -Name $(Join-Path -Path $PSScriptRoot -ChildPath "upload_app")
 
 $localSetupFiles = $null
-$config = "..\apps\7z\1.0.0.0\info.yml"
+$config = "..\apps\seven-zed\1.0.0.0\info.yml"
 
 Add-Application $localSetupFiles $config
