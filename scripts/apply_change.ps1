d4f679ed83a09afd6b10c03a29f64ee4266ef382
igor.dopita <igor.dopita@adt.com.au>
2022-05-26 15:45:52 +1000

    Adding 7-zip

--- scripts/apply_change.ps1
+++ scripts/apply_change.ps1
@@ -1,6 +1,6 @@
 Import-Module -Name $(Join-Path -Path $PSScriptRoot -ChildPath "upload_app")
 
 $localSetupFiles = $null
-$config = "..\apps\7zip\20.7.0.0\info.yml"
+$config = "..\apps\7-zip\20.7.0.0\info.yml"
 
 Add-Application $localSetupFiles $config
